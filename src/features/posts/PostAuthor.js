ca09b65282ddbc1e3b378735f914ae7fd2aa321c
Harshita222 <kumariharsita501@gmail.com>
2024-07-06 00:02:23 +0530

    do

---
+++ src/features/posts/PostAuthor.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+// import { selectUserById } from '../users/usersSlice'
+import { selectUserById } from '../users/userSlice'
+
+
+export const PostAuthor = ({ userId }) => {
+  const author = useSelector((state) => selectUserById(state, userId))
+
+  return <span>by {author ? author.name : 'Unknown author'}</span>
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.css'
+// import App from './App'
+// import store from './app/store'
+// import { Provider } from 'react-redux'
+
+// import { fetchUsers } from './features/users/userSlice'
 
 
+// import './api/server'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// store.dispatch(fetchUsers())
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
+
+import { fetchUsers } from './features/users/userSlice';
+
+import './api/server'; // Ensure this imports your server setup correctly
+
+store.dispatch(fetchUsers());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
