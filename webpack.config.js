1ef018e69c3aa6841862eb96471d3a8be65bf77f
Harshita222 <kumariharsita501@gmail.com>
2024-07-06 00:08:44 +0530

    my

--- webpack.config.js
+++ webpack.config.js
@@ -38,30 +38,43 @@
 //         extensions: ['.js', '.jsx'],
 //     }
 // };
-const path = require('path');
-
+const path = require("path");
+const HtmlWebpackPlugin= require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname,"/dist"),
+        filename: "index_bundle.js",
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$|\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
 };
